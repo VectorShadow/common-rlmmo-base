cfabfd680eafd112aeaa43ba0e73bc11ab5010cb
vsdl <vectorshadowdigitallabs@gmail.com>
2023-09-17 18:07:25 -0500

    Improved test coverage

--- src/test/java/org/vsdl/common/rlmmo/maintainable/world/MaintainableFieldTest.java
+++ src/test/java/org/vsdl/common/rlmmo/maintainable/world/MaintainableFieldTest.java
@@ -2,7 +2,10 @@ package org.vsdl.common.rlmmo.maintainable.world;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.vsdl.common.mmo.consistency.MaintenanceTransaction;
+import org.vsdl.common.mmo.consistency.MaintenanceTransactionRecord;
 import org.vsdl.common.rl.world.asset.Actor;
+import org.vsdl.common.rl.world.map.Field;
 import org.vsdl.common.rlmmo.maintainable.MaintainableFixture;
 import org.vsdl.common.rlmmo.maintainable.world.asset.MaintainableActor;
 import org.vsdl.common.rlmmo.maintainable.world.map.MaintainableField;
@@ -30,6 +33,23 @@ public class MaintainableFieldTest {
         field.initialize();
         MaintainableActor player = MaintainableFixture.getMaintainableActor_Player();
         field.addActor(player);
+        List<Actor> actors = field.getActorsAt(player.getLocation());
+        assertNotNull(actors);
+        assertEquals(1, actors.size());
+    }
+
+    @Test
+    void testAddOneActorViaMaintenanceTransaction() {
+        field.initialize();
+        MaintainableActor player = MaintainableFixture.getMaintainableActor_Player();
+        MaintenanceTransactionRecord record = MaintenanceTransactionRecord.initializeRecord(field.getUUID(), field.getVersion());
+        MaintenanceTransaction transaction = new MaintenanceTransaction(Field.class.getCanonicalName(), "addActor", new Class[]{Actor.class}, new Object[]{player});
+        record.record(transaction);
+        try {
+            record.applyTo(field);
+        } catch (Exception e) {
+            fail();
+        }
         List<Actor> actors = field.getActorsAt(player.getLocation());
         assertNotNull(actors);
         assertEquals(1, actors.size());
